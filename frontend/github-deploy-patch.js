62e5a765758992a3715e455092ad55486be63757
David Komer <6406986+dakom@users.noreply.github.com>
2024-05-30 16:50:55 +0200

    actions

--- frontend/github-deploy-patch.js
+++ frontend/github-deploy-patch.js
@@ -6,20 +6,20 @@ const fs = require("fs");
 const path = require("path");
 
 (async () => {
-    const ROOTS = ["css", "media"];
-
-    const uri_root = "w3bay"
-
-
     let data = await readAsync("./dist/index.html");
-    if(uri_root !== "") {
-        for(const root of ROOTS) {
-            data = data.replaceAll(`"/${root}`, `"/${uri_root}/${root}`);
-            data = data.replaceAll(`'/${root}`, `'/${uri_root}/${root}`);
-        }
-    }
+    // const ROOTS = ["css", "media"];
 
-    await writeAsync("./dist/index.html", data);
+    // const uri_root = "w3bay"
+
+
+    // if(uri_root !== "") {
+    //     for(const root of ROOTS) {
+    //         data = data.replaceAll(`"/${root}`, `"/${uri_root}/${root}`);
+    //         data = data.replaceAll(`'/${root}`, `'/${uri_root}/${root}`);
+    //     }
+    // }
+
+    //await writeAsync("./dist/index.html", data);
     await writeAsync("./dist/404.html", data);
 })();
 
